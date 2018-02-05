0f4edfea9e51a8070df346b330eaba827f7d2002
jacas <jacek.budzynski.92@gmail.com>
2018-02-05 20:15:51 +0000

    connected contact form to firebase

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,12 +3,12 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import {Injectable} from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { Message } from '../../logic/Message';
-import { MessageService } from '../../logic/MessageService';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import {Message} from '../../logic/Message';
+import {MessageService} from '../../logic/MessageService';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -25,8 +25,7 @@ declare var grecaptcha: any;
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit
-{
+export class ContactComponent implements OnInit {
   options: FormGroup;
   matcher = new MyErrorStateMatcher();
   message: Message;
@@ -61,12 +60,10 @@ export class ContactComponent implements OnInit
   }
 
   onSend() {
-    if(grecaptcha.getResponse() === '')
-    {
+    if (grecaptcha.getResponse() === '') {
       console.log('Recaptcha failed');
     }
-    else
-    {
+    else {
       this.messageService.insertMessage(this.message);
       console.log(this.message);
       console.log(grecaptcha.getResponse());
