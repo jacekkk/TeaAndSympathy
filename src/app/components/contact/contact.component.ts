8d97deaa3d8d7f715f3364f114205360bbdfe2bd
jacas <jacek.budzynski.92@gmail.com>
2018-02-07 15:25:42 +0000

    fixed sh!t3

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,14 +3,14 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import {Injectable} from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { Message } from '../../logic/Message';
-import { MessageService } from '../../logic/MessageService';
-import { MatSnackBar } from '@angular/material';
-import {MatDialog , MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import {Message} from '../../logic/Message';
+import {MessageService} from '../../logic/MessageService';
+import {MatSnackBar} from '@angular/material';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {text} from '@angular/core/src/render3/instructions';
 
 /** Error when invalid control is dirty, touched, or submitted. */
@@ -29,8 +29,7 @@ declare var grecaptcha: any;
   styleUrls: ['./contact.component.css'],
 })
 
-export class ContactComponent implements OnInit
-{
+export class ContactComponent implements OnInit {
   options: FormGroup;
   matcher = new MyErrorStateMatcher();
   message: Message;
@@ -56,17 +55,16 @@ export class ContactComponent implements OnInit
     Validators.pattern('^([+]?\\d{1,2}[-\\s]?|)\\d{3}[-\\s]?\\d{3}[-\\s]?\\d{4}$'),
   ]);
 
-   // tracks the value message text area in the form
-   messageFormControl = new FormControl('', [
-     Validators.required,
-     Validators.maxLength(1000)
-   ]);
+  // tracks the value message text area in the form
+  messageFormControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(1000)
+  ]);
 
   constructor(fb: FormBuilder,
               private messageService: MessageService,
               private snackBar: MatSnackBar,
-              public dialog: MatDialog)
-  {
+              public dialog: MatDialog) {
     this.options = fb.group({
       hideRequired: false,
       floatLabel: 'auto',
@@ -78,16 +76,16 @@ export class ContactComponent implements OnInit
   }
 
   // resets form to its original state
-  resetForm(){
-      this.emailFormControl.reset();
-      this.emailFormControl.setErrors(null);
-      this.nameFormControl.reset();
-      this.nameFormControl.setErrors(null);
-      this.phoneFormControl.reset();
-      this.phoneFormControl.setErrors(null);
-      this.messageFormControl.reset();
-      this.messageFormControl.setErrors(null);
-      grecaptcha.reset();
+  resetForm() {
+    this.emailFormControl.reset();
+    this.emailFormControl.setErrors(null);
+    this.nameFormControl.reset();
+    this.nameFormControl.setErrors(null);
+    this.phoneFormControl.reset();
+    this.phoneFormControl.setErrors(null);
+    this.messageFormControl.reset();
+    this.messageFormControl.setErrors(null);
+    grecaptcha.reset();
   }
 
   // handles user's attempt to submit the form once he clicks "Send" button
@@ -95,27 +93,24 @@ export class ContactComponent implements OnInit
     console.log(this.message);
 
     // check if captcha validation was successful
-    if(grecaptcha.getResponse() === '')
-    {
+    if (grecaptcha.getResponse() === '') {
       console.log('Recaptcha failed: ' + grecaptcha.getResponse());
-      this.dialogText = "Failed to send, please check your input and try again.";
+      this.dialogText = 'Failed to send, please check your input and try again.';
     }
-    else if(this.message.subject == null || this.message.name == null || this.message.email == null
-    && this.message.body)
-    {
-      this.dialogText = "Failed to send, please check your input and try again.";
+    else if (this.message.subject == null || this.message.name == null || this.message.email == null
+      && this.message.body) {
+      this.dialogText = 'Failed to send, please check your input and try again.';
     }
-    else
-    {
+    else {
       this.messageService.insertMessage(this.message);
       this.resetForm();
 
-      this.dialogText = "Message sent, thank you. We will be in touch shortly.";
+      this.dialogText = 'Message sent, thank you. We will be in touch shortly.';
     }
 
     // open dialog after user has attempted to send the form
     let dialogRef = this.dialog.open(DialogContactForm, {
-      data: { text : this.dialogText }
+      data: {text: this.dialogText}
     });
 
     // scale the dialog automatically
@@ -128,16 +123,15 @@ export class ContactComponent implements OnInit
 }
 
 
-
 // dialog component
 @Component({
   selector: 'dialog-contact-form',
   templateUrl: 'dialog-contact-form.html',
 })
 export class DialogContactForm {
-  constructor(
-    public dialogRef: MatDialogRef<DialogContactForm>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(public dialogRef: MatDialogRef<DialogContactForm>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
