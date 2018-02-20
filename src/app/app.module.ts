67761918fd55ab97eccc9ec369efd8241fa342f9
BenassiJosef <benassisolutions@gmail.com>
2018-02-20 11:14:32 +0000

    MASONARY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,8 @@ import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+
+
 import {
   MatAutocompleteModule,
   MatButtonModule,
