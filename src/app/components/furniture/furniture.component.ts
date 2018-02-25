309715b769849a6aa84d8636295da0b6a7a8599c
jacas <jacek.budzynski.92@gmail.com>
2018-02-25 21:48:35 +0000

    styled dialog for images

--- src/app/components/furniture/furniture.component.ts
+++ src/app/components/furniture/furniture.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {Entry} from 'contentful';
 import {ContentfulService} from '../../contentful.service';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'furniture',
@@ -35,7 +36,7 @@ export class FurnitureComponent implements OnInit {
     });
 
     // scale the dialog automatically
-    // dialogRef.updateSize('auto', 'auto');
+     dialogRef.updateSize('auto', 'auto');
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
@@ -47,11 +48,13 @@ export class FurnitureComponent implements OnInit {
 @Component({
   selector: 'dialog-furniture-product',
   templateUrl: 'dialog-furniture-product.html',
-  styleUrls: ['./dialog-furniture-product.css']
+  styleUrls: ['./dialog-furniture-product.css'],
+  providers: [NgbCarouselConfig]
 })
 export class DialogFurnitureProduct {
   constructor(public dialogRef: MatDialogRef<DialogFurnitureProduct>,
-              @Inject(MAT_DIALOG_DATA) public data: any) {
+              @Inject(MAT_DIALOG_DATA) public data: any, config: NgbCarouselConfig) {
+    config.interval = 10000;
   }
 
   onNoClick(): void {
